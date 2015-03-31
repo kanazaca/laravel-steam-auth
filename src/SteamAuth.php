09cd5b2d39656f90b2892f03075fd3be31650ba9
Carlos Florencio <carlosmflorencio@gmail.com>
2015-03-31 19:33:42 +0100

    Some tweaks

--- src/SteamAuth.php
+++ src/SteamAuth.php
@@ -1,50 +1,91 @@
 <?php namespace kanazaca\LaravelSteamAuth;
 
+use Config;
+
 class SteamAuth implements SteamAuthInterface
 {
-
-    private $OpenID;
-    public $steamInfo;
+    private $openID;
+    private $steamID;
+    private $steamInfo;
 
     const OPENID_URL = 'https://steamcommunity.com/openid';
+    const STEAM_INFO_URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s';
 
     public function __construct(LightOpenID $openID)
     {
-        $this->OpenID = $openID;
-        $this->OpenID->identity = self::OPENID_URL;
+        $this->openID = $openID;
+        $this->openID->identity = static::OPENID_URL;
         $this->init();
     }
 
     private function init()
     {
-        if ($this->OpenID->mode == 'cancel') {
-
-            $this->steamInfo = FALSE;
-
-        } else if ($this->OpenID->mode) {
-
-            if ($this->OpenID->validate()) {
-
-                $this->steamInfo = new SteamInfo($this->OpenID->identity);
-
+        if($this->openID->mode == 'cancel')
+        {
+            $this->steamID = false;
+        }
+        else if($this->openID->mode)
+        {
+            if($this->openID->validate())
+            {
+                $this->steamID = basename($this->openID->identity);
             }
-
+            else
+            {
+                $this->steamID = false;
+            }
         }
     }
 
-    public function validate()
-    {
-        return $this->steamInfo ? TRUE : FALSE;
+    /**
+     * Gets the steaminfo of that id
+     * @return bool|SteamInfo
+     */
+    public function getSteamInfo() {
+        if(!$this->validate()) return false;
+
+        $json = file_get_contents(sprintf(static::STEAM_INFO_URL, Config::get('steam-auth.steam_api_key'), $this->steamID));
+        $json = json_decode($json, TRUE);
+        return new SteamInfo($json["response"]["players"][0]);
     }
 
+    /**
+     * Checks the steam login
+     *
+     * @return bool
+     */
+    public function validate()
+    {
+        return $this->steamID ? true : false;
+    }
+
+    /**
+     * Returns the redirect response to login
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function redirect()
     {
         return redirect($this->url());
     }
 
+    /**
+     * Returns the login url
+     *
+     * @return String
+     */
     public function url()
     {
-        return $this->OpenID->authUrl();
+        return $this->openID->authUrl();
+    }
+
+    /**
+     * Returns the steam id
+     *
+     * @return bool|string
+     */
+    public function getSteamId(){
+        return $this->SteamID;
     }
 
 }
--- src/SteamInfo.php
+++ src/SteamInfo.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace kanazaca\LaravelSteamAuth;
+
 use Config;
 
 /**
@@ -9,102 +10,39 @@ use Config;
  */
 class SteamInfo
 {
-
-    /**
-     * @var
-     */
-    protected $id;
-    /**
-     * @var
-     */
+    protected $steamID;
     protected $nick;
-    /**
-     * @var
-     */
     protected $lastLogin;
-    /**
-     * @var
-     */
     protected $profileURL;
-    /**
-     * @var
-     */
     protected $profilePicture;
-    /**
-     * @var
-     */
     protected $profilePictureMedium;
-    /**
-     * @var
-     */
     protected $profilePictureFull;
-    /**
-     * @var
-     */
     protected $name;
-    /**
-     * @var
-     */
     protected $clanID;
-    /**
-     * @var
-     */
     protected $createdAt;
+    protected $countryCode;
 
-    /**
-     * @var
-     */
-    protected $all;
-
-    /**
-     * @var string
-     */
-    protected $url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s';
-
-    function __construct($id)
+    function __construct($data)
     {
-        $this->id = $id;
-
-        $this->getJson(); //Get steamInfo from json and save it to $this->all
-        $this->setId($this->all["steamid"]);
-        $this->setNick($this->all["personaname"]);
-        $this->setLastLogin($this->all["lastlogoff"]);
-        $this->setProfileURL($this->all["profileurl"]);
-        $this->setProfilePicture($this->all["avatar"]);
-        $this->setProfilePictureMedium($this->all["avatarmedium"]);
-        $this->setProfilePictureFull($this->all["avatarfull"]);
-        $this->setName($this->all["realname"]);
-        $this->setClanID($this->all["primaryclanid"]);
-        $this->setCreatedAt($this->all["timecreated"]);
-    }
-
-
-    /**
-     * @return mixed
-     */
-    protected function getJson()
-    {
-        $json = file_get_contents(sprintf($this->url, Config::get('steam-auth.steam_api_key'), $this->id));
-        $json = json_decode($json, TRUE);
-        $steam = $json["response"]["players"][0];
-
-        $this->all = $steam;
+        $this->steamID              = $data["steamid"];
+        $this->nick                 = $data["personaname"];
+        $this->lastLogin            = $data["lastlogoff"];
+        $this->profileURL           = $data["profileurl"];
+        $this->profilePicture       = $data["avatar"];
+        $this->profilePictureMedium = $data["avatarmedium"];
+        $this->profilePictureFull   = $data["avatarfull"];
+        $this->name                 = $data["realname"];
+        $this->clanID               = $data["primaryclanid"];
+        $this->createdAt            = $data["timecreated"];
+        $this->countryCode          = $data["loccountrycode"];
     }
 
     /**
      * @return mixed
      */
-    public function getId()
+    public function getSteamID()
     {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
+        return $this->steamID;
     }
 
     /**
@@ -116,27 +54,11 @@ class SteamInfo
     }
 
     /**
-     * @param mixed $nick
-     */
-    public function setNick($nick)
-    {
-        $this->nick = $nick;
-    }
-
-    /**
      * @return mixed
      */
     public function getLastLogin()
     {
         return $this->lastLogin;
-    }
-
-    /**
-     * @param mixed $lastLogin
-     */
-    public function setLastLogin($lastLogin)
-    {
-        $this->lastLogin = $lastLogin;
     }
 
     /**
@@ -148,27 +70,11 @@ class SteamInfo
     }
 
     /**
-     * @param mixed $profileURL
-     */
-    public function setProfileURL($profileURL)
-    {
-        $this->profileURL = $profileURL;
-    }
-
-    /**
      * @return mixed
      */
     public function getProfilePicture()
     {
         return $this->profilePicture;
-    }
-
-    /**
-     * @param mixed $profilePicture
-     */
-    public function setProfilePicture($profilePicture)
-    {
-        $this->profilePicture = $profilePicture;
     }
 
     /**
@@ -180,27 +86,11 @@ class SteamInfo
     }
 
     /**
-     * @param mixed $profilePictureMedium
-     */
-    public function setProfilePictureMedium($profilePictureMedium)
-    {
-        $this->profilePictureMedium = $profilePictureMedium;
-    }
-
-    /**
      * @return mixed
      */
     public function getProfilePictureFull()
     {
         return $this->profilePictureFull;
-    }
-
-    /**
-     * @param mixed $profilePictureFull
-     */
-    public function setProfilePictureFull($profilePictureFull)
-    {
-        $this->profilePictureFull = $profilePictureFull;
     }
 
     /**
@@ -212,27 +102,11 @@ class SteamInfo
     }
 
     /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
      * @return mixed
      */
     public function getClanID()
     {
         return $this->clanID;
-    }
-
-    /**
-     * @param mixed $clanID
-     */
-    public function setClanID($clanID)
-    {
-        $this->clanID = $clanID;
     }
 
     /**
@@ -244,12 +118,10 @@ class SteamInfo
     }
 
     /**
-     * @param mixed $createdAt
+     * @return mixed
      */
-    public function setCreatedAt($createdAt)
+    public function getCountryCode()
     {
-        $this->createdAt = $createdAt;
+        return $this->countryCode;
     }
-
-
 }
