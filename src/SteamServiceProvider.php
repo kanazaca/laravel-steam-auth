305bbe9bd09e5542deb04ed7c9a01ed8045a93b7
mokujinsan <mokujinsan@gmail.com>
2015-08-09 12:55:25 +0100

    Update SteamServiceProvider.php

--- src/SteamServiceProvider.php
+++ src/SteamServiceProvider.php
@@ -1,4 +1,4 @@
-<?php namespace mokujin\LaravelSteamAuth;
+<?php namespace mokujinsan\LaravelSteamAuth;
 
 use Config;
 use Illuminate\Support\ServiceProvider;
@@ -23,14 +23,14 @@ class SteamServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('mokujin\LaravelSteamAuth\LightOpenID', function($app)
+        $this->app->singleton('mokujinsan\LaravelSteamAuth\LightOpenID', function($app)
         {
             $redirect = Config::get('steam-auth.redirect_url') ?: Config::get('app.url');
             return new LightOpenID(url($redirect));
         });
 
         $this->app['steamauth'] = $this->app->share(function ($app) {
-            return new SteamAuth($app->make('mokujin\LaravelSteamAuth\LightOpenID'));
+            return new SteamAuth($app->make('mokujinsan\LaravelSteamAuth\LightOpenID'));
         });
     }
 
